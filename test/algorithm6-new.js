0e8e679a9e64941cc2d3fa377cdb7adf19b47626
qyang7 <12046023+qyang777@user.noreply.gitee.com>
2026-05-27 11:01:36 +0800

    888 #comment feat:哈哈嘻嘻 #CMBDevClaw

---
+++ test/algorithm6-new.js
@@ -0,0 +1,34 @@
+function climbStairs(n) {
+  if (n <= 2) return n;
+
+  let first = 1;
+  let second = 2;
+
+  for (let i = 3; i <= n; i++) {
+    const third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return second;
+}
+
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfitValue = 0;
+
+  for (const price of prices) {
+    if (price < minPrice) minPrice = price;
+    else maxProfitValue = Math.max(maxProfitValue, price - minPrice);
+  }
+
+  return maxProfitValue;
+}
+
+function singleNumber(nums) {
+  let result = 0;
+  for (const num of nums) result ^= num;
+  return result;
+}
+
+module.exports = { climbStairs, maxProfit, singleNumber };
